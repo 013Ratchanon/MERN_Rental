ced9c8043452742214fe72df6503f447e890d2bd
013Ratchanon <dewtete@gmail.com>
2026-02-01 18:14:31 +0700

    auth

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
-
+// Register, Login, and Verify routes
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/verify", authController.verify);
