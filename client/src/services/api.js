a315aa89c19098b4d742687024d54584b2af349b
013Ratchanon <dewtete@gmail.com>
2026-02-01 18:23:38 +0700

    api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // ปรับตาม backend ของคุณ
+  baseURL: 'https://mern-rental-2.onrender.com/api', // ปรับตาม backend ของคุณ
   headers: {
     'Content-Type': 'application/json',
   },
